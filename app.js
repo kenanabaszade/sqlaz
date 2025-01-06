1d9ed219f1b8b78ddef9202284d42bb1723501a5
kenanabaszada <kenan.abaszada@pragmaticdlt.com>
2025-01-06 17:01:54 +0400

    changes

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
- 
+const detailsContainer = document.getElementById("detailsContainer");
+const premiumContainer = document.getElementById("premiumAboutUs");
+const showPremiumBtn = document.getElementById("showPremiumBtn");
+const goBackBtn = document.getElementById("goBackBtn");
+showPremiumBtn.addEventListener("click", () => {
+  detailsContainer.classList.add("fade-out");
+
+  detailsContainer.addEventListener("transitionend", onDetailsFadeOut, { once: true });
+
+  function onDetailsFadeOut() {
+    detailsContainer.style.display = "none";
+
+    console.log(premiumContainer.classList);
+    premiumContainer.classList.add("active");
+    premiumContainer.offsetHeight;
+    premiumContainer.classList.add("fade-in");
+  }
+});
+  

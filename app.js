c81af7aaebab8c3c964aa96fc1acfe8b6ae563f2
kenanabaszada <kenan.abaszada@pragmaticdlt.com>
2025-01-06 09:34:37 +0400

    changes

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-// Add form submission functionality
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for your message!');
-});
+ 
